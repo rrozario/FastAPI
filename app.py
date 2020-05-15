66fefd6a70f489cbc849f13f46d5f03ae9e6c3c4
Ruchita Robert Rozario <61220014+rrozario@users.noreply.github.com>
2020-05-15 16:41:28 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+# Core Pkg
+from fastapi import FastAPI
+import uvicorn
+import json
+import csv
+import pandas as pd
+
+# Init
+app = FastAPI(debug=True)
+
+people = []
+with open('people.csv','rU') as f: 
+    reader = csv.DictReader(f)
+    for row in reader:
+        people.append(row)
+
+#Output a Json file with Birth Month as Keys and Names as values
+@app.get('/people')
+async def get_people():
+	df=pd.DataFrame(people)	
+	dict1=dict(df.groupby("month").apply(lambda x:x['name'].tolist()))
+	return {"people":dict1}
+
+#Output names that have the corresponding user-inputed birth month
+@app.get('/people/{month}')
+async def get_person(month):
+	person = [person for person in people if person["month"] == month]
+	return {"people":person}
+
+if __name__ == '__main__':
+	uvicorn.run(app, host="127.0.0.1",port="8000")
